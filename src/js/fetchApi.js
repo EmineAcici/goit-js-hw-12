fd523be04160f999f546642efe9c58a61671546e
EmineBusraAcici <eminebusraacici@hotmail.com>
2026-03-28 19:25:11 +0300

    Edit files

--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = "55070456-620b3af2625ed4d2721553348";
 
-export async function fetchImage(query) {
+export async function fetchImage(query,page=1) {
   try {
     const response = await axios.get("https://pixabay.com/api/",{
     params : {
@@ -11,6 +11,8 @@ export async function fetchImage(query) {
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
+    page: page,
+    per_page: 40,
   },
     });
     return response.data;
--- src/js/main.js
+++ src/js/main.js
@@ -8,35 +8,72 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 const loadingMessage = document.querySelector('.loading-text');
+const loadMoreBtn = document.querySelector('.load-more-button');
 
-getSearchQuery(async (query) => {
-  clearGallery();
-  loadingMessage.classList.remove('is-hidden');
+let currentQuery = '';
+let currentPage;
 
+async function handleFetch() {
   try {
-    const data = await fetchImage(query);
+    loadingMessage.classList.remove('is-hidden');
+    loadMoreBtn.classList.add('is-hidden');
 
+    const data = await fetchImage(currentQuery, currentPage);
     const images = data.hits;
+    const totalHits = data.totalHits;
 
     if (!images || images.length === 0) {
       iziToast.error({
-          title: 'Error',
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          position: 'topRight',
-        });
+        title: 'Error',
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+        position: 'topRight',
+      });
+      return;
+    }
+    updateGallery(images);
+
+    if (totalHits > currentPage * 40) {
+      loadMoreBtn.classList.remove('is-hidden');
     } else {
-      updateGallery(images);
+      iziToast.info({
+        message: "We're sorry, but you've reached the end of search results.",
+        position: 'topRight',
+      });
     }
   } catch (error) {
     iziToast.error({
-        title: 'Error',
-        message:
-          'Something went wrong while connecting to the server. Please try again later!',
-        position: 'topRight',
-      });
-      console.error(error);
+      title: 'Error',
+      message:
+        'Something went wrong while connecting to the server. Please try again later!',
+      position: 'topRight',
+    });
+    console.error(error);
   } finally {
     loadingMessage.classList.add('is-hidden');
   }
+}
+
+getSearchQuery(async query => {
+  currentQuery = query;
+  currentPage = 1;
+  clearGallery();
+  loadMoreBtn.classList.add('is-hidden');
+  loadingMessage.classList.remove('is-hidden');
+
+  await handleFetch();
+});
+
+loadMoreBtn.addEventListener('click', async () => {
+  currentPage++;
+  await handleFetch();
+
+  const { height: cardHeight } = document
+    .querySelector('.result-gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 });
